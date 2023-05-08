d73020ccbb114e7712955e215e690ea6012a6525
logeshwari-10 <131449811+logeshwari-10@users.noreply.github.com>
2023-05-08 15:25:44 +0530

    Add files via upload

---
+++ EducationalLoan/src/test/java/com/Eduloan/demo/EducationalLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eduloan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
